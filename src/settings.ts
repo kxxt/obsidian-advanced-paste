d4f8f53d49921297ce87a6c80f1ce6b8def891e5
kxxt <rsworktech@outlook.com>
2023-10-20 15:24:48 +0800

    do not enhance default paste by default
    Obsidian 1.4 has upgraded the paste experience to a good enough level.
    Turning this on by default might cause more trouble than good.

--- src/settings.ts
+++ src/settings.ts
@@ -24,7 +24,7 @@ const DEFAULT_SETTINGS: AdvancedPasteSettings = {
         linkReferenceStyle: "full",
         // preformattedCode: false,
     },
-    enhanceDefaultPaste: true,
+    enhanceDefaultPaste: false,
     // autoLinkTitleRegex:
 };
 
